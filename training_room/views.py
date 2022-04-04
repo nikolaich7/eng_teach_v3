a36a525687c4365f9757bdc46e9aad43a2897ec6
nikolaich7 <nikolaich7777777@gmail.com>
2022-04-04 09:06:52 +0300

    graph

--- training_room/views.py
+++ training_room/views.py
@@ -1,5 +1,9 @@
 from datetime import datetime, date
 from time import sleep
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+import matplotlib.dates
 
 from django.contrib.auth.models import User
 from django.http import HttpResponse
@@ -74,3 +78,43 @@ def training(form_data, type_training):
         'type_training': type_training,
     }
     return context
+
+
+def graph(request):
+    date_training = []
+    right = []
+    wrong = []
+    right_w = []
+    wrong_w = []
+    user = User.objects.get(pk=1)
+    print(user)
+    tasks = DateHistory.objects.filter(user=user)
+    print(tasks)
+    for task in tasks:
+        date_training.append(task.date)
+        right.append(task.right_answers_sentences)
+        wrong.append(task.wrong_answers_sentences)
+        right_w.append(task.right_answers_words)
+        wrong_w.append(task.wrong_answers_words)
+    print(date_training, right)
+
+    date_float_r = matplotlib.dates.date2num(date_training) - 0.2
+    date_float_w = matplotlib.dates.date2num(date_training) + 0.2
+    fig, axes = pylab.subplots(2, 1)
+
+
+    axes[0].xaxis.set_major_formatter (matplotlib.dates.DateFormatter("%d.%m"))
+    axes[1].xaxis.set_major_formatter (matplotlib.dates.DateFormatter("%d.%m"))
+    axes[0].bar(date_float_r, right, width=0.4)
+    axes[0].bar(date_float_w, wrong, width=0.4)
+    axes[1].bar(date_float_r, right_w, width=0.4)
+    axes[1].bar(date_float_w, wrong_w, width=0.4)
+
+    axes[0].set_facecolor('seashell')
+    axes[1].set_facecolor('seashell')
+
+    #plt.savefig('1.png')
+    plt.show()
+    return HttpResponse('done')
+
+
